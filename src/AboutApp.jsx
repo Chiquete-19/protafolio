7631bb3727df59d13e07a74302465b40002e86ad
Fernando Chiquete <ferchiquete2003@gmail.com>
2026-03-18 13:46:20 -0700

    vercel json no jalaaaa

--- src/AboutApp.jsx
+++ src/AboutApp.jsx
@@ -3,7 +3,7 @@ import About from "./components/sections/About"
 function AboutApp() {
   return (
     <div className="bg-slate-950 text-white overflow-x-hidden">
-      <About/ >
+      <About />
     </div>
   )
 }
